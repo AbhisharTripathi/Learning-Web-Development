1fbc2c7d1844caa811f9869aafddcba89fa0cfdf
Abhishar Tripathi <ldcabhisharTripathi@gmail.com>
2026-04-02 09:43:59 +0530

    Improved the initializing facility.

--- 41-55 AirBnb/init/index.js
+++ 41-55 AirBnb/init/index.js
@@ -1,8 +1,15 @@
+if(process.env.NODE_ENV != "production") {
+    require("dotenv").config();
+}
+
 const mongoose = require("mongoose");
 const Listing = require("../model/listing.js");
 const initData = require("./data.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dns = require("dns");
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+const MONGO_URL = process.env.MONGO_URL;
 main().then(() => {
     console.log("Connected to db")
 })
@@ -16,7 +23,7 @@ async function main() {
 const initDB = async () => {
     try {
         await Listing.deleteMany({});
-        initData.data = initData.data.map((obj)  => ({...obj, owner: '69ca4de045f4647beefc84d4'}));
+        initData.data = initData.data.map((obj)  => ({...obj, owner: '69ca4de045f4647beefc84d4'}));//Clear the whole database then Signup a user first then get the ObjectId of that user and paste it in the value of owner.
         await Listing.insertMany(initData.data);
         console.log("Database initialized with sample data.");
     }
